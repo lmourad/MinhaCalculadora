c96df9f3af0ea44df8ecea29f904e5b2637a874a
moura <moura@DESKTOP-MQ5TSLO>
2023-03-01 00:11:10 -0300

    Adicionar arquivos de projeto.

---
+++ MinhaCalculadora/WindowAdmin.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MinhaCalculadora
+{
+    /// <summary>
+    /// Lógica interna para WindowAdmin.xaml
+    /// </summary>
+    public partial class WindowAdmin : Window
+    {
+        public WindowAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_criarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            CriarUsuarioWindow objCriarUsuarioWindow = new CriarUsuarioWindow();
+            this.Visibility = Visibility.Hidden;
+            objCriarUsuarioWindow.Show();
+        }
+    }
+}
